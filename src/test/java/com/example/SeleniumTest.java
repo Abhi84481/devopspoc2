334b7ae4f642d1c04a41db84342e5b4d0cc80dde
Prabhat Kumar <44310215+prabhatu012345@users.noreply.github.com>
2024-09-04 19:34:52 +0530

    Update SeleniumTest.java

--- src/test/java/com/example/SeleniumTest.java
+++ src/test/java/com/example/SeleniumTest.java
@@ -1,27 +1,14 @@
-package com.example;
+package dev.selenium.hello;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
 
 public class SeleniumTest {
-    private WebDriver driver;
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
 
-    @BeforeClass
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-        driver = new ChromeDriver();
-    }
+        driver.get("https://selenium.dev");
 
-    @Test
-    public void testSelenium() {
-        driver.get("https://www.selenium.dev/");
-    }
-
-    @AfterClass
-    public void tearDown() {
         driver.quit();
     }
 }
