b8a1760a7456db2fa750b4e6d75024f8e55c0f16
Prabhat Kumar <44310215+prabhatu012345@users.noreply.github.com>
2024-09-06 17:58:35 +0530

    Update SeleniumTest.java

--- src/test/java/com/example/SeleniumTest.java
+++ src/test/java/com/example/SeleniumTest.java
@@ -1,18 +1,23 @@
 package dev.selenium.hello;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumTest {
     private WebDriver driver;
 
     @BeforeEach
     public void setUp() {
+        // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "/home/jenkins/agent/workspace/");
+
         // Initialize the WebDriver
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver(options);
     }
 
     @Test
